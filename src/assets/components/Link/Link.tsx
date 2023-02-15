5073c892225eb6ef09cf18b6639487d745c36121
Hiteazel <khacminhbrandon.vo@gmail.com>
2023-02-15 11:35:23 +0100

    inital link

---
+++ src/assets/components/Link/Link.tsx
@@ -0,0 +1,11 @@
+import "./Link.css";
+// import React from "react";
+// import { Link } from "react-router-dom";
+
+function Link() {
+  return (
+    <div className="Link">
+      <p>Already signed in ? Log here !</p>
+    </div>
+  );
+}

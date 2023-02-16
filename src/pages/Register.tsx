bf15e33640c773b3b6c92f89467b0b2288d08b2b
Antoine Azevedo Da Silva <azevedo-a@hotmail.fr>
2023-02-16 16:03:46 +0100

    Add register

--- src/pages/Register.tsx
+++ src/pages/Register.tsx
@@ -1,3 +1,60 @@
+import clsx from "clsx";
+import { useState } from "react";
+
+import { Button } from "../components/Button/Button";
+import { TextInput } from "../components/TextInput/TextInput";
+
+import "../assets/css/pages/login.css";
+
 export const Register = () => {
-  return <></>;
+  const [demoButtonText, setDemoButtonText] = useState("REGISTER");
+
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [firstname, setFirstname] = useState("");
+  const [lastname, setLastname] = useState("");
+
+  const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    setDemoButtonText("LOADING...");
+    setTimeout(() => {
+      setDemoButtonText("REGISTER");
+    }, 5000);
+    console.log("submit");
+  };
+
+  return (
+    <section id="login">
+      <div className={clsx("split")}></div>
+      <div className={clsx("split", "login-form-bg")}>
+        <form>
+          <TextInput
+            label="Email"
+            title="Your mail"
+            value={email}
+            updateValue={setEmail}
+          />
+          <TextInput
+            label="Password"
+            title="Your password"
+            value={password}
+            updateValue={setPassword}
+          />
+          <TextInput
+            label="Lastname"
+            title="Your lastname"
+            value={lastname}
+            updateValue={setLastname}
+          />
+          <TextInput
+            label="Firstname"
+            title="Your firstname"
+            value={firstname}
+            updateValue={setFirstname}
+          />
+          <Button text={demoButtonText} action={handleSubmit} />
+        </form>
+      </div>
+    </section>
+  );
 };

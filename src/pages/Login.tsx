cff352ba4ee50d7b9cd98fa4889278d71f042e3b
Antoine Azevedo Da Silva <azevedo-a@hotmail.fr>
2023-02-16 15:55:20 +0100

    Start login

--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -1,3 +1,46 @@
+import clsx from "clsx";
+import { useState } from "react";
+
+import { Button } from "../components/Button/Button";
+import { TextInput } from "../components/TextInput/TextInput";
+
+import "../assets/css/pages/login.css";
+
 export const Login = () => {
-  return <></>;
+  const [demoButtonText, setDemoButtonText] = useState("LOGIN");
+
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    setDemoButtonText("LOADING...");
+    setTimeout(() => {
+      setDemoButtonText("LOGIN");
+    }, 5000);
+    console.log("submit");
+  };
+
+  return (
+    <section id="login">
+      <div className={clsx("split")}></div>
+      <div className={clsx("split", "login-form-bg")}>
+        <form>
+          <TextInput
+            label="Email"
+            title="Your mail"
+            value={email}
+            updateValue={setEmail}
+          />
+          <TextInput
+            label="Password"
+            title="Your password"
+            value={password}
+            updateValue={setPassword}
+          />
+          <Button text={demoButtonText} action={handleSubmit} />
+        </form>
+      </div>
+    </section>
+  );
 };
